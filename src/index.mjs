f237dbd28258f7a863253f956acb837f7f5c1e13
aimen08 <aimen_hamza@hotmail.com>
2023-03-29 17:44:30 +0100

    add server and chat

--- src/index.mjs
+++ src/index.mjs
@@ -1,30 +1,67 @@
-import { create } from "@open-wa/wa-automate";
 import { openai } from "./config/index.mjs";
 
-create({
-  sessionId: "GPT-3",
-  multiDevice: false,
-  authTimeout: 60,
-  blockCrashLogs: true,
-  disableSpins: true,
-  headless: true,
-  hostNotificationLang: "PT_BR",
-  logConsole: false,
-  popup: true,
-  qrTimeout: 0,
-}).then((client) => start(client));
+import wp from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
 
-function start(client) {
-  client.onMessage(async (message) => {
-    const { body } = message;
+// import server from "server";
 
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: body,
+// const { get, post } = server.router;
+// const { json } = server.reply;
+
+// // Launch server with options and a couple of routes
+// server({ port: 8044 }, [
+//   get("/", (ctx) =>
+//     json({
+//       name: "test",
+//     })
+//   ),
+// ]);
+
+const client = new wp.Client({ authStrategy: new wp.LocalAuth() });
+
+client.on("qr", (qr) => {
+  try {
+    qrcode.generate(qr, { small: true });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message", async (message) => {
+  const { body } = message;
+  client.sendSeen(message.from);
+  // client.getContacts().then((contacts) => {
+  //   console.log(contacts);
+  // });
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant that only answers questions",
+        },
+        { role: "user", content: body },
+      ],
       temperature: 0.5,
-      max_tokens: 200,
+      max_tokens: 700,
     });
 
-    await client.sendText(message.from, completion.data.choices[0].text);
-  });
-}
+    await client.sendMessage(
+      message.from,
+      completion.data.choices[0].message.content
+    );
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+client.initialize();
+
+
+
